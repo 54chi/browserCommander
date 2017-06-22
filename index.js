3474fd5dc73022bf4caec8b6af0269ca5f20bd74
Sachi Shirasaka <sachi.shirasaka@cloudcraze.com>
2017-06-22 16:57:44 -0500

    refresh of code

--- index.js
+++ index.js
@@ -6,16 +6,23 @@
 */
 
 var http = require("http");
+var qs = require("querystring");
 const opn = require('opn');
+var testUrl="";
 
 http.createServer(function(req, res){
+
+	//length of http://localhost:3030?url=
 	res.writeHead(301, {
-		"location" : "http://54chi.com"
+		"location" : decodeURIComponent(testUrl)
 	});
+
+	console.log(res);
 	res.end();
+
 }).listen(3030, "127.0.0.1");
 
-console.log("server redirects from localhost to technotip.org")
-
+testUrl=encodeURIComponent("https://cloudcraze.atlassian.net/secure/RapidBoard.jspa?rapidView=55&view=detail&quickFilter=365#");
+console.log(testUrl);
 // Opens the url in the default browser
-//opn('http://localhost:3030', {app: 'firefox', wait:false});
+opn('http://localhost:3030', {wait:false});
